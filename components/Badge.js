2f7b9858f109c39543083dd9f961ad49ef07fdf4
wahidari <wahiid.ari@gmail.com>
2022-03-12 23:28:06 +0700

    update color

--- components/Badge.js
+++ components/Badge.js
@@ -10,7 +10,7 @@ export default function Badge({ variant, children }) {
         {children}
       </span>
     : variant == "warning" ?
-      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded bg-yellow-200 text-yellow-600">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded bg-yellow-200 text-yellow-500">
         {children}
       </span>
     : variant == "info" ?
@@ -21,7 +21,7 @@ export default function Badge({ variant, children }) {
       <span className="text-sm font-semibold mr-2 px-2 py-1 rounded bg-gray-200 text-gray-700">
         {children}
       </span>
-    : 
+    :
       <span className="text-sm font-semibold mr-2 px-2 py-1 rounded bg-blue-200 text-blue-600">
         {children}
       </span>
--- components/BadgeOutline.js
+++ components/BadgeOutline.js
@@ -2,27 +2,27 @@ export default function BadgeOutline({ variant, children }) {
 
   return (
     variant == "success" ?
-      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-green-500 dark:text-green-800 border border-green-500 dark:border-green-500">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-green-500 border border-green-500">
         {children}
       </span>
     : variant == "danger" ?
-      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-red-500 dark:text-red-800 border border-red-500 dark:border-red-500">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-red-500 border border-red-500">
         {children}
       </span>
     : variant == "warning" ?
-      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-yellow-500 dark:text-yellow-800 border border-yellow-500 dark:border-yellow-500">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-yellow-500 border border-yellow-500">
         {children}
       </span>
     : variant == "info" ?
-      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-orange-500 dark:text-orange-800 border border-orange-500 dark:border-orange-500">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-orange-500 border border-orange-500">
         {children}
       </span>
     : variant == "dark" ?
-      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-gray-700 dark:text-gray-100 border border-gray-500 dark:border-gray-500">
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-gray-700 dark:text-gray-300 border border-gray-500">
         {children}
       </span>
-    : 
-      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-blue-500 dark:text-blue-800 border border-blue-500 dark:border-blue-500">
+    :
+      <span className="text-sm font-semibold mr-2 px-2 py-1 rounded text-blue-500 border border-blue-500">
         {children}
       </span>
   )
